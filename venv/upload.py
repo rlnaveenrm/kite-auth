c2a1ce0d9346aff83463b84c5a368a693b70dc28
lakshman <akshan.ram4@gmail.com>
2020-06-01 10:23:50 +0200

    check

--- venv/upload.py
+++ venv/upload.py
@@ -29,6 +29,17 @@ doc_ref = db.collection(u'stocks').document(stock_id).collection(u'nse').documen
 
 
 while True:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    bse = {}
+    nse = {}
+    price = soup.select('.span_price_wrap')
+    last_updated = soup.select('.display_lastupd')
+    bse['price'] = price[0].text
+    nse['price'] = price[1].text
+    bse['last_updated'] = last_updated[0].text
+    nse['last_updated'] = last_updated[1].text
+    print(nse['last_updated'])
     doc_ref.set({nse['last_updated']: nse['price']}, merge=True)
     time.sleep(60)
 
