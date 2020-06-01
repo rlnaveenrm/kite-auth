7127949a36b3e44cc1c76446e49d91cba7a754a2
lakshman <akshan.ram4@gmail.com>
2020-06-01 10:41:20 +0200

    bug check

--- venv/upload.py
+++ venv/upload.py
@@ -13,19 +13,7 @@ db = firestore.client()
 url = 'https://www.moneycontrol.com/india/stockpricequote/computers-software/infosys/IT'
 stock_id = url.split('/')[-2]
 print(stock_id)
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
-bse = {}
-nse = {}
-price = soup.select('.span_price_wrap')
-last_updated = soup.select('.display_lastupd')
-bse['price'] = price[0].text
-nse['price'] = price[1].text
-bse['last_updated'] = last_updated[0].text
-nse['last_updated'] = last_updated[1].text
-print(nse['last_updated'])
 
-doc_ref = db.collection(u'stocks').document(stock_id).collection(u'nse').document(nse['last_updated'].split(',')[0])
 
 
 while True:
@@ -40,6 +28,7 @@ while True:
     bse['last_updated'] = last_updated[0].text
     nse['last_updated'] = last_updated[1].text
     print(nse['last_updated'])
+    doc_ref = db.collection(u'stocks').document(stock_id).collection(u'nse').document(nse['last_updated'].split(',')[0])
     doc_ref.set({nse['last_updated']: nse['price']}, merge=True)
     time.sleep(60)
 
